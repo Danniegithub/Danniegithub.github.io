d4c7589cba7cec9fa20fffaefccbff75b1d99f11
AndersonHJB <bornforthis@bornforthis.cn>
2022-09-09 12:02:25 +0800

    update

---
+++ bornforthis/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,4 @@
+export const pagesData = {
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/huangjiabao/GitHub/WebSite/Danniegithub.github.io/bornforthis/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+}
