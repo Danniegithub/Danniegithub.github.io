d22a6ec5da605f8450b3f24f95448b22508fc11a
AndersonHJB <bornforthis@bornforthis.cn>
2022-09-09 12:11:20 +0800

    update

--- danniebook/.vuepress/config.ts
+++ danniebook/.vuepress/config.ts
@@ -7,7 +7,7 @@ export default defineUserConfig({
   title: "笔记网站",
   description: "AI悦创编程一对一,少儿编程一对一教学,成人,大学生,小学生,人工智能,数据分析,数据结构等一对一教学,微信:Jiabcdefh",
 
-  base: "/Notebook/",
+  base: "/",
 
   plugins: [
     searchPlugin({
